7595d27169f25541b03adba638b796451c619fa8
Ivor Wanders <ivor@iwanders.net>
2023-09-02 20:11:39 +0000

    Print some information from the hook.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
 
 // use windows_sys;
-use windows::Win32::UI::WindowsAndMessaging::{SetWindowsHookExA, CallNextHookEx, WH_KEYBOARD_LL, HOOKPROC, GetMessageW, MSG};
+use windows::Win32::UI::WindowsAndMessaging::{SetWindowsHookExA, CallNextHookEx, WH_KEYBOARD_LL, HOOKPROC, GetMessageW, MSG, KBDLLHOOKSTRUCT, WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP };
 use windows::Win32::System::LibraryLoader::LoadLibraryA;
 use windows::Win32::Foundation::{WPARAM, LPARAM, LRESULT};
 
+/// code: A code the hook procedure uses to determine how to process the message. If nCode is less than zero, the hook procedure must pass the message to the CallNextHookEx function without further processing and should return the value returned by CallNextHookEx. This parameter can be one of the following values.
+/// Value	Meaning
+/// HC_ACTION 0	
+/// The wParam and lParam parameters contain information about a keyboard message.
+/// wparam: The identifier of the keyboard message. This parameter can be one of the following messages: WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, or WM_SYSKEYUP.
+/// lparam: Type: LPARAM
+/// A pointer to a KBDLLHOOKSTRUCT structure.
 unsafe extern "system" fn foo(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     println!("things!: {code:?}");
+    if code < 0 {
+        return CallNextHookEx(0, code, wparam, lparam);
+    }
+
+    let z = std::mem::transmute::<_, *const KBDLLHOOKSTRUCT >(lparam);
+    println!("z.vkCode: {}", (*z).vkCode);
+    println!("z.scanCode: {}", (*z).scanCode);
+    println!("z.flags: 0x{:x?}", (*z).flags);
+    println!("time: {:?}", (*z).time);
+
+    match wparam as u32 {
+        WM_KEYDOWN => {println!("down");},
+        WM_KEYUP =>  {println!("up");},
+        WM_SYSKEYDOWN => {println!("sys down");},
+        WM_SYSKEYUP => {println!("sys up");},
+        _ => {}
+    }
     return CallNextHookEx(0, code, wparam, lparam);
     0
 }
+/*
+
+*/
 
 pub fn main() {
 
@@ -22,12 +49,14 @@ pub fn main() {
         println!("hh: 0x{hh:x?}");
 
         loop {
+            println!("looping");
             std::thread::sleep(std::time::Duration::from_millis(100));
-            // let mut message: MSG = mem::zeroed();
+            // https://stackoverflow.com/a/65571485
+            // This hook is called in the context of the thread that installed it. The call is made by sending a message to the thread that installed the hook. Therefore, the thread that installed the hook must have a message loop.
             let mut message: MSG = std::mem::zeroed();
             GetMessageW(&mut message, 0, 0, 0);
-
         }
     }
-    println!("slfjdslkfj");
+    println!("");
 }
+    
