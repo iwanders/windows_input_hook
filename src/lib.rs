4abd0ff23301ff619f6f6fc2741efbf024b00a51
Ivor Wanders <ivor@iwanders.net>
2023-09-03 13:14:31 +0000

    Proper callback structure.

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,11 @@ use windows::Win32::UI::Input::KeyboardAndMouse::{
 /// lparam: Type: LPARAM
 /// A pointer to a KBDLLHOOKSTRUCT structure.
 unsafe extern "system" fn foo(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    // println!("things!: {code:?}");
     if code < 0 {
         return CallNextHookEx(HHOOK(0), code, wparam, lparam);
     }
 
-    let current_id = GetCurrentThreadId();
+    // let current_id = GetCurrentThreadId();
     // println!("current id; {current_id}");
     let z = std::mem::transmute::<_, *const KBDLLHOOKSTRUCT>(lparam);
     // println!("z.vkCode: {}", (*z).vkCode);
@@ -32,22 +31,10 @@ unsafe extern "system" fn foo(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRES
     // println!("time: {:?}", (*z).time);
 
     let action = match wparam.0 as u32 {
-        WM_KEYDOWN => {
-            // println!("down");
-            KeyAction::Down
-        }
-        WM_KEYUP => {
-            // println!("up");
-            KeyAction::Up
-        }
-        WM_SYSKEYDOWN => {
-            // println!("sys down");
-            KeyAction::Down
-        }
-        WM_SYSKEYUP => {
-            // println!("sys up");
-            KeyAction::Up
-        }
+        WM_KEYDOWN => KeyAction::Down,
+        WM_KEYUP => KeyAction::Up,
+        WM_SYSKEYDOWN => KeyAction::Down,
+        WM_SYSKEYUP => KeyAction::Up,
         _ => {
             panic!("unsupported key action {}", wparam.0);
         }
@@ -58,9 +45,8 @@ unsafe extern "system" fn foo(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRES
         code: (*z).vkCode as u8,
     };
     InputHook::MAP.with(|z| {
-        let l = z.lock();
-        // let m = z.borrow();
-        if let Some(f) = l.expect("cannot be poisoned").get(&input) {
+        let mut l = z.borrow_mut();
+        if let Some(f) = l.get_mut(&input) {
             f(input);
         }
     });
@@ -70,7 +56,6 @@ unsafe extern "system" fn foo(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRES
 
 pub unsafe fn dump_keys() {
     for i in 0..256 {
-        //
         let mut v = String::new();
         for _ in 0..64 {
             v.insert_str(0, " ");
@@ -107,33 +92,34 @@ impl KeyInput {
     }
 }
 
-use std::sync::Arc;
-use std::sync::Mutex;
-type HookFunction = dyn Fn(KeyInput) + Send;
+type HookFunction = dyn FnMut(KeyInput) + Send;
 type HookHandler = Box<HookFunction>;
 type HookMap = std::collections::HashMap<KeyInput, HookHandler>;
 
 use std::cell::RefCell;
-use std::sync::atomic::AtomicU32;
+use std::sync::atomic::{AtomicBool, AtomicU32};
 pub struct InputHook {
     thread: Option<std::thread::JoinHandle<()>>,
     thread_id: std::sync::Arc<AtomicU32>,
+    running: std::sync::Arc<AtomicBool>,
 }
 impl InputHook {
     thread_local! {
-        static MAP: Mutex<HookMap> = Mutex::new(Default::default());
+        static MAP: RefCell<HookMap> = RefCell::new(Default::default());
     }
 
     pub fn new(map: HookMap) -> Self {
         let thread_id = std::sync::Arc::new(AtomicU32::new(0));
+        let running = std::sync::Arc::new(AtomicBool::new(true));
         // let mutexed_map = Mutex::new(map);
         let tid = thread_id.clone();
+        let t_running = running.clone();
         let thread = Some(std::thread::spawn(move || {
             unsafe {
                 // Store the hook map;
                 InputHook::MAP.with(|z| {
-                    let l = z.lock();
-                    *l.expect("cannot be poisoned") = map;
+                    let mut l = z.borrow_mut();
+                    *l = map;
                 });
 
                 // Store the thread id such that we can later exit this thread.
@@ -146,25 +132,37 @@ impl InputHook {
                     .expect("hook did not succeed");
 
                 // https://stackoverflow.com/a/65571485
-                // This hook is called in the context of the thread that installed it. The call is made by sending a message to the thread that installed the hook. Therefore, the thread that installed the hook must have a message loop.
-                let mut message: MSG = std::mem::zeroed();
-                GetMessageW(&mut message, HWND(0), 0, 0);
-                println!("Shutting down");
+                // This hook is called in the context of the thread that installed it. The call is made by sending a message to the thread that installed the hook. Therefore, the thread that installed the hook must have a message loop, which we place here.
+                while t_running.load(std::sync::atomic::Ordering::Relaxed) {
+                    let mut message: MSG = std::mem::zeroed();
+                    GetMessageW(&mut message, HWND(0), 0, 0);
+                    std::thread::sleep(std::time::Duration::from_millis(1));
+                }
 
                 // Unhook the things.
                 UnhookWindowsHookEx(hh).expect("unhook did not succeed");
             }
         }));
-        InputHook { thread, thread_id }
+        InputHook {
+            thread,
+            thread_id,
+            running,
+        }
     }
 }
 impl Drop for InputHook {
     fn drop(&mut self) {
-        // First, let the message drop out of the GetMessage block by sending it a  message.
+        // Set the boolean to stop running.
+        self.running
+            .store(false, std::sync::atomic::Ordering::Relaxed);
+
         unsafe {
+            // Send a message to the thread to drop out of the GetMessage loop.
             let tid = self.thread_id.load(std::sync::atomic::Ordering::Relaxed);
             PostThreadMessageA(tid, 0, WPARAM(0), LPARAM(0)).expect("other thread must be running");
         }
+
+        // Finally, join the thread.
         let t = self.thread.take();
         t.unwrap().join().expect("join should succeed");
     }
@@ -180,8 +178,8 @@ pub fn main() {
             println!("A: {v:?}");
         }),
     );
-    let hh = InputHook::new(map);
-    for i in 0..50 {
+    let _h = InputHook::new(map);
+    for _i in 0..50 {
         std::thread::sleep(std::time::Duration::from_millis(100));
     }
 }
